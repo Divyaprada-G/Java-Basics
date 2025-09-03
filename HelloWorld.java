49e203188d7e28fdda9995ed7fb2b16c1ec7f9a2
Divyaprada G <162574253+Divyaprada-G@users.noreply.github.com>
2025-09-03 13:23:23 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("This is My New Programming Language");
+    }
+}
+
+
+// # How to Run This Hello World
+
+// 1-> Click on New Terminal
+// 2-> Type in Terminal [javac HelloWorld.java]
+// 3-> Then Type [java HelloWorld]
+// 4-> The Code Run Successfully
+
+// 5-> Short Hand Code to Run Java Program is:- [java HelloWorld.java]
